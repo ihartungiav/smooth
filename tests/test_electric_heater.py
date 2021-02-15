087e3506dc62106bff0d47dd47463a077b23d093
stefan.schirmeister <stefan.schirmeister@localiser.de>
2021-02-15 17:13:40 +0100

    unit tests for electric heater and electrolyzer

---
+++ tests/test_electric_heater.py
@@ -0,0 +1,24 @@
+from smooth.components.component_electric_heater import ElectricHeater
+import oemof.solph as solph
+
+
+def test_init():
+    acdc = ElectricHeater({})
+
+    params = {"power_max": 100}
+    acdc = ElectricHeater(params)
+    assert acdc.power_max == params["power_max"]
+
+def test_create_oemof_model():
+    acdc = ElectricHeater({
+        "bus_th": "bus_el",
+        "bus_el": "bus_th"
+    })
+    model = acdc.create_oemof_model({
+        "bus_el": solph.Bus(label="bus_el"),
+        "bus_th": solph.Bus(label="bus_th"),
+    }, None)
+
+    assert type(model) == solph.Transformer
+    assert len(model.inputs) == 1
+    assert len(model.outputs) == 1
---
+++ tests/test_electrolyzer.py
@@ -0,0 +1,91 @@
+from smooth.components.component_electrolyzer import Electrolyzer
+from smooth.framework.simulation_parameters import SimulationParameters
+import oemof.solph as solph
+from oemof.outputlib import processing
+
+import pytest
+
+
+class TestBasic:
+
+    sim_params = SimulationParameters({"interval_time": 30})
+
+    def test_init(self):
+        ely = Electrolyzer({"power_max": 100, "sim_params": self.sim_params})
+        assert ely.energy_max == 50 # 100kW, 30 minutes
+        assert ely.max_production_per_step is not None
+
+    def test_create_oemof_model(self):
+        ely = Electrolyzer({
+            "bus_el": "bus1",
+            "bus_h2": "bus2",
+            "sim_params": self.sim_params
+        })
+        model = ely.create_oemof_model({
+            "bus1": solph.Bus(label="bus1"),
+            "bus2": solph.Bus(label="bus2")
+        }, None)
+        assert type(model) == solph.custom.PiecewiseLinearTransformer
+        assert len(model.inputs) == 1
+        assert len(model.outputs) == 1
+
+    def test_update_non_linear_behaviour(self):
+        ely = Electrolyzer({"sim_params": self.sim_params})
+        ely.update_nonlinear_behaviour()
+
+        # test supporting points. Assumes default values (apart from interval_time)
+        # should any of these fail, have fun testing the associated functions
+
+        n_supporting_point = 10
+        temp = [295, 299, 303, 307, 310, 314, 317, 320, 323, 326, 327]
+        h2 = [0, 103, 196, 283, 366, 445, 521, 594, 665, 733, 800]
+        energy = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+
+        for i in range(n_supporting_point + 1):
+            assert temp[i] == int(ely.supporting_points["temperature"][i])
+            assert h2[i] == int(ely.supporting_points["h2_produced"][i] * 1000)
+            assert energy[i] == int(ely.supporting_points["energy"][i] / 1000)
+
+
+class TestUpdate:
+
+    sim_params = SimulationParameters({"interval_time": 30, "n_intervals": 2})
+    sim_params.i_interval = 0
+    oemof_model = solph.EnergySystem(
+        timeindex=sim_params.date_time_index[0:1],
+        freq='{}min'.format(sim_params.interval_time)
+    )
+
+    def test_update_states(self):
+        # simulate one smooth iteration
+        # build energy model with two batteries, connected by a bus
+        bus_el = solph.Bus(label="bus_el")
+        bus_h2 = solph.Bus(label="bus_h2")
+        self.oemof_model.add(bus_el)
+        self.oemof_model.add(bus_h2)
+
+        ely = Electrolyzer({
+            "bus_el": "bus_el",
+            "bus_h2": "bus_h2",
+            "sim_params": self.sim_params
+        })
+        ely_model = ely.create_oemof_model({
+            "bus_el": bus_el,
+            "bus_h2": bus_h2
+        }, None)
+        self.oemof_model.add(ely_model)
+
+        model_to_solve = solph.Model(self.oemof_model)
+
+        # solve model
+        oemof_results = model_to_solve.solve(solver='cbc', solve_kwargs={'tee': False})
+        assert oemof_results["Solver"][0]["Status"].key == "ok"
+        results = processing.results(model_to_solve)
+        assert results is not None
+
+        old_temp = ely.temperature
+        ely.update_states(results)
+        # idle: cooling down, no water consumption
+        assert ely.temperature < old_temp
+        assert ely.states["temperature"][0] == ely.temperature
+        assert ely.states["water_consumption"][0] == 0
